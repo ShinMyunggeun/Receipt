760505d4bac469721c1ef334b37e3556b5834592
ShinMyunggeun <86578532+ShinMyunggeun@users.noreply.github.com>
2021-07-02 08:45:00 +0900

    Add files via upload

---
+++ 영수증 - OrderListArray.java
@@ -0,0 +1,18 @@
+package standard3;
+
+public class OrderListArray {
+	int no;
+	String name;
+	int price;
+	int count;
+	int total;
+	
+	public void OrderListClass(int no, String name, int price, int count, int total) {
+		this.no = no;
+		this.name = name;
+		this.price = price;
+		this.price = price; 
+		this.count = count;
+		this.total = total;
+}
+}
